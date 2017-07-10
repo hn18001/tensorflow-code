322ff17e76cdefffa1f225aeb49bdb2ba197dc95
hn18001 <hn18001@gmail.com>
2017-07-10 17:03:14 +0800

    fix bug

--- chapter5/5-3.py
+++ chapter5/5-3.py
@@ -23,39 +23,39 @@ images_test, labels_test = cifar10_input.inputs(eval_data=True,
 
 # input placeholder
 image_holder = tf.placeholder(tf.float32, [batch_size, 24, 24, 3])
-label_holder = tf.placeholder(tf.float32, [batch_size])
+labels_holder = tf.placeholder(tf.float32, [batch_size])
 
 # conv1
 weight1 = variable_with_weight_loss([5,5,3,64], stddev=5e-2, w1=0.0)
-bias1 = tf.Variable(tf.constants(0.0, shape=[64]))
-kernel1 = tf.conv2d(image_holder, weight1, [1,1,1,1], padding='SAME')
+bias1 = tf.Variable(tf.constant(0.0, shape=[64]))
+kernel1 = tf.nn.conv2d(image_holder, weight1, [1,1,1,1], padding='SAME')
 conv1 = tf.nn.relu(tf.nn.bias_add(kernel1, bias1))
 pool1 = tf.nn.max_pool(conv1, ksize=[1,3,3,1], strides=[1,2,2,1], padding='SAME')
 norm1 = tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001/9.0, beta=0.75)
 
 # conv2
 weight2 = variable_with_weight_loss([5,5,64,64], stddev=5e-2, w1=0.0)
-bias2 = tf.Variable(tf.constants(0.1, [64]))
+bias2 = tf.Variable(tf.constant(0.1, shape=[64]))
 kernel2 = tf.nn.conv2d(norm1, weight2,[1,1,1,1], padding='SAME')
 conv2 = tf.nn.relu(tf.nn.bias_add(kernel2, bias2))
-norm2 = tf.nn.lrn(conv2, 4, bias=1.0, aplha=0.001/9.0, beta=0.75)
+norm2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001/9.0, beta=0.75)
 pool2 = tf.nn.max_pool(norm2, ksize=[1,3,3,1], strides=[1,2,2,1], padding='SAME')
 
 # fc1
 reshape = tf.reshape(pool2, [batch_size, -1])
 dim = reshape.get_shape()[1].value
 weight3 = variable_with_weight_loss([dim, 384], stddev=0.04, w1 = 0.004)
-bias3 = tf.Variable(tf.constants(0.1, shape=[384]))
-local3 = tf.nn.relu(tf.matmul(reshape, weights), + biase3)
+bias3 = tf.Variable(tf.constant(0.1, shape=[384]))
+local3 = tf.nn.relu(tf.matmul(reshape, weight3) + bias3)
 
 # fc2
 weight4 = variable_with_weight_loss([384,192], stddev=0.04, w1=0.004)
-bias4 = tf.Variable(tf.constants(0.1, shape=[192]))
+bias4 = tf.Variable(tf.constant(0.1, shape=[192]))
 local4 = tf.nn.relu(tf.matmul(local3, weight4) + bias4)
 
 # last layer
 weight5 = variable_with_weight_loss([192,10], stddev=1/192.0, w1=0.0)
-bias5 = tf.Variable(tf.constants(0.0, shape=[10]))
+bias5 = tf.Variable(tf.constant(0.0, shape=[10]))
 logits = tf.add(tf.matmul(local4, weight5), bias5)
 
 # loss
@@ -68,12 +68,13 @@ def loss(logits, labels):
 	return tf.add_n(tf.get_collection('losses'), name='total_loss')
 
 loss = loss(logits, labels_holder)
-train_op = tf.train.AdamOptimizer(1e-3).minimize(losses)
+train_op = tf.train.AdamOptimizer(1e-3).minimize(loss)
 
-top_k_op = tf.nn.in_top_k(logits, label_holder, 1)
+labels_holder1 = tf.cast(labels_holder, tf.int64)
+top_k_op = tf.nn.in_top_k(logits, labels_holder1, 1)
 
 sess = tf.InteractiveSession()
-tf.global_variables_initializer.run()
+tf.global_variables_initializer().run()
 
 tf.train.start_queue_runners()
 
@@ -81,7 +82,7 @@ tf.train.start_queue_runners()
 for step in range(max_steps):
 	start_time = time.time()
 	image_batch, label_batch = sess.run([images_train, labels_train])
-	_, loss_value = sess.run([train_op, loss], feed_dict={image_holder:image_batch, label_holder:label_batch})
+	_, loss_value = sess.run([train_op, loss], feed_dict={image_holder:image_batch, labels_holder:label_batch})
 	duration = time.time() - start_time
 
 	if step % 10 == 0:
@@ -89,7 +90,7 @@ for step in range(max_steps):
 		sec_per_batch = float(duration)
 
 		format_str = ('step %d, loss=%.2f (%.1f examples/sec; %.3f sec/batch)')
-		print(format_str % (step, loss, examples_per_sec, sec_per_batch))
+		print(format_str % (step, loss_value, examples_per_sec, sec_per_batch))
 
 # test
 num_examples = 10000
@@ -100,10 +101,10 @@ total_sample_count = num_iter * batch_size
 step = 0
 while step < num_iter:
 	image_batch, label_batch = sess.run([images_test, labels_test])
-	predictions = sess.run([top_k_op], feed_dict ={image_holder:image_batch, label_holder:label_batch})
+	predictions = sess.run([top_k_op], feed_dict ={image_holder:image_batch, labels_holder:label_batch})
 
 	true_count += np.sum(predictions)
 	step+=1
 
-precision = true_count / total_sample_count
+precision = 1.0 * true_count / total_sample_count
 print('precision @ 1 = %.3f' % precision)
