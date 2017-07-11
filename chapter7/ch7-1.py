ade87688bd03d72e6323b61ea26de250f3f04cae
何能（He Neng） <heneng@iqiyi.pps>
2017-07-11 11:27:13 +0800

    add code

--- chapter7/ch7-1.py
+++ chapter7/ch7-1.py
@@ -29,3 +29,31 @@ def read_data(filename):
 
 words = read_data(filename)
 print("Data size", len(words))
+
+# create vocabulary
+vocabulary_size = 50000
+
+def build_dataset(words):
+	count = [['UNK', -1]]
+	count.extend(collections.Counter(words).most_common(vocabulary_size-1))
+	dictionary = dict()
+	for word, _ in count:
+		dictionary[word] = len(dictionary)
+	data = list()
+	unk_count = 0
+	for word in words:
+		if word in dictionary:
+			index = dictionary[word]
+		else:
+			index = 0
+			unk_count += 1
+		data.append(index)
+	count[0][1] = unk_count
+	reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+	return data, cont, dictionary, reverse_dictionary
+
+data, count, dictionary, reverse_dictionary = build_dataset(words)
+
+del words
+print('Most common words (+UNK)', count[:5])
+print('Sample data', data[:10], [reverse_dictionary[i] for i in data[:10]])
