87dc032b3d072d6237a44fa0761eabcead075b65
何能（He Neng） <heneng@iqiyi.pps>
2017-07-11 14:38:31 +0800

    add code

--- chapter7/ch7-1.py
+++ chapter7/ch7-1.py
@@ -57,3 +57,34 @@ data, count, dictionary, reverse_dictionary = build_dataset(words)
 del words
 print('Most common words (+UNK)', count[:5])
 print('Sample data', data[:10], [reverse_dictionary[i] for i in data[:10]])
+
+data_index = 0
+def generate_batch(batch_size, num_skip, skip_window):
+	global data_index
+	assert batch_size % num_skips == 0
+	assert num_skips <= 2 * skip_window
+	batch = np.ndarray(shape=(batch_size), dtype=np.int32)
+	labels = np.ndarray(shape=(batch_size,1), dtype=np.int32)
+	span = 2*skip_windows + 1
+	buffer = collections.deque(maxlen=span)
+
+	for _ in range(span):
+		buffer.append(data[data_index])
+		data_index = (data_index + 1) % len(data)
+
+	for i in range(batch_size // num_skips):
+		target = skip_window
+		targets_to_avoid = [skip_window]
+		for j in range(num_skips):
+			while target in targets_to_avoid:
+				target = random.randint(0, span-1)
+			targets_to_avoid.append(target)
+			batch[i*num_skips + j] = buffer[skip_window]
+			labels[i*num_skips + j, 0] = buffer[target]
+		buffer.append(data[data_index])
+		data_index = (data_index + 1) % len(data)
+	return batch, labels
+
+batch, labels = generate_batch(batch_size=8, num_skips=2, skip_window=1)
+for i in range(8):
+	print(batch[i], reverse_dictionary[batch[i]], '->', labels[i,0], reverse_dictionary[labels[i,0]])
