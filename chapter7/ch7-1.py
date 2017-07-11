2cefee91cd70bd043168a347396fa20a047ddbea
何能（He Neng） <heneng@iqiyi.pps>
2017-07-11 15:44:39 +0800

    add code

--- chapter7/ch7-1.py
+++ chapter7/ch7-1.py
@@ -125,6 +125,15 @@ with graph.as_default():
 
 num_steps = 10001
 
+def plot_with_labels(low_dim_embs, labels, filename='tsne.png'):
+    assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
+    plt.figure(figsize=(18,18))
+    for i, label in enumerate(labels):
+        x,y = low_dim_embs[i, :]
+        plt.scatter(x,y)
+        plt.annotate(label, xy=(x,y), xytext=(5,2), textcoords='offset points', ha = 'right', va='bottom')
+    plt.savefig(filename)
+
 with tf.Session(graph=graph) as session:
 	init.run()
 	print("initialized")
@@ -156,20 +165,10 @@ with tf.Session(graph=graph) as session:
 				print(log_str)
 		final_embeddings = normalized_embeddings
 
-
-def plot_with_labels(low_dim_embs, labels, filename='tsne.png'):
-    assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
-    plt.figure(figsize=(18,18))
-    for i, label in enumerate(labels):
-        x,y = low_dim_embs[i, :]
-        plt.scatter(x,y)
-        plt.annotate(label, xy=(x,y), xytext=(5,2), textcoords='offset points', ha = 'right', va='bottom')
-    plt.savefig(filename)
-
-from sklearn.manifold import TSNE
-import matplotlib.pyplot as plt
-tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
-plot_only = 100
-low_dim_embs = tsne.fit_transform(final_embeddings[:plot_only, :])
-labels = [reverse_dictionary[i] for i in range(plot_only)]
-plot_with_labels(low_dim_embs, labels)
+		from sklearn.manifold import TSNE
+		import matplotlib.pyplot as plt
+		tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+		plot_only = 100
+		low_dim_embs = tsne.fit_transform(final_embeddings[:plot_only, :])
+		labels = [reverse_dictionary[i] for i in range(plot_only)]
+		plot_with_labels(low_dim_embs, labels)
